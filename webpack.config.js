4611ce4f3e736ff3b913c081c41f3ecebe7f1455
c0nrast3d <ryhor.pauliuts@gmail.com>
2017-12-11 13:03:49 +0100

    Add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: ["./index.js"],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+  contentBase: path.resolve(__dirname, 'dist/'),
+  },
+};
