c8c05a7d9093ab87ece295e295808e97cfa7a406
c0nrast3d <ryhor.pauliuts@gmail.com>
2017-12-12 10:53:29 +0100

    Add some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,26 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+
   context: path.resolve(__dirname, 'src'),
   entry: ["./index.js"],
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
   },
